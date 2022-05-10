0fb59b68864ef2a74cd76d146e59098a962b1939
Marcos Spomberg <marcosspomberg@gmail.com>
2022-05-10 00:46:36 +0000

    Add users seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'faker'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times do |i|
+  User.create!(user_name: Faker::Internet.username, email: Faker::Internet.email, password: Faker::Internet.password, location: Faker::Address.full_address)
+end
