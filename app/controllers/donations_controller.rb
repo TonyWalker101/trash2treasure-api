1d49a13ee1992d2dfe76a9873d804fe9358af412
Marcos Spomberg <marcosspomberg@gmail.com>
2022-05-10 21:58:05 +0000

    Create donations controller

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,2 +1,8 @@
 class DonationsController < ApplicationController
+  def show
+  end
+
+  def index
+    @donations = Donation.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 
+  root to: 'donations#index'
+
   resources :donations, only: [:index, :show, :new, :destroy] do
     post :add_donation
     post :remove_donation
   end
 
-  get '/donations' => 'donations#show'
+  get '/donations' => 'donations#index'
   post '/add-new' => 'donations#create'
   post '/claim-donation' => 'donations#destroy'
 
