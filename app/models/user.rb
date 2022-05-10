41a076cac7a0e5f848c17480e53eaa5fc28b73c4
Marcos Spomberg <marcosspomberg@gmail.com>
2022-05-10 00:45:40 +0000

    Fix users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :donations
   has_many :comments
 
-  validates :name, presence: true
+  validates :user_name, presence: true
   validates :email, presence: true 
   validates :location, presence: true
   validates :password, presence: true
