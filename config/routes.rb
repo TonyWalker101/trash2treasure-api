8c7892968965ab476a585e37cbb938f828e965d7
Marcos Spomberg <marcosspomberg@gmail.com>
2022-05-12 22:32:14 +0000

    Fix add donation route

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,17 @@ Rails.application.routes.draw do
 
   root to: 'donations#index'
 
-  resources :donations, only: [:index, :show, :new, :destroy] do
-    post :add_donation
-    post :remove_donation
-  end
+  resources :donations, only: [:index, :show, :new, :destroy]
 
-  resources :users, :comments, only: [:index, :show]
+  resources :users, only: [:index, :show]
+
+  resources :comments, only: [:index, :show, :new]
 
   get '/donations' => 'donations#index'
   get '/users' => 'users#index'
   get '/comments' => 'comments#index'
 
-  # post '/add-new' => 'donations#create'
-  # post '/claim-donation' => 'donations#destroy'
+  post '/add-donation' => 'donations#new'
+  post '/claim-donation' => 'donations#destroy'
 
 end
