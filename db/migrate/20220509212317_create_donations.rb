8e3edb2e06c5c35aef04370728382f76dfbe1c02
Marcos Spomberg <marcosspomberg@gmail.com>
2022-05-13 20:59:38 +0000

    Fix migrations

--- db/migrate/20220509212317_create_donations.rb
+++ db/migrate/20220509212317_create_donations.rb
@@ -2,14 +2,14 @@ class CreateDonations < ActiveRecord::Migration[6.1]
   def change
     create_table :donations do |t|
       t.references :user, index: true, foreign_key: true
-      t.name :string
-      t.location :text
-      t.description :text
-      t.condition :string
-      t.available :boolean
-      t.latitude :decimal
-      t.longitude :decimal
-      t.image :string
+      t.string :name
+      t.text :location
+      t.text :description
+      t.string :condition
+      t.boolean :available
+      t.decimal :latitude
+      t.decimal :longitude
+      t.string :image
 
       t.timestamps null:false
     end
