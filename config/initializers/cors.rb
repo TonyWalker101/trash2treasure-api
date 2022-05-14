b912ef7c5a215598b0955ca97746434585839323
Tony <awalker780@gmail.com>
2022-05-14 14:17:39 -0400

    Add rack-cors package to project

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,4 +3,4 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins 'http://localhost:3000'
     resource '*', headers: :any, methods: [:get, :post, :patch, :put]
   end
-end
+end 
