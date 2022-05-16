172c694f1a16f302dcb12b75e11352d80d785eb7
Marcos Spomberg <marcosspomberg@gmail.com>
2022-05-16 00:54:29 +0000

    Implement add-comment method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,4 +3,14 @@ class CommentsController < ApplicationController
     comments = Comment.all
     render json: comments.to_json
   end
+
+  def new
+    user = User.find_by(id: 1)
+    comment = user.comments.new(
+      donation_id: params[:donation_id],
+      comment: params[:comment],
+      created_at: Time.now
+    )
+    comment.save!
+  end
 end
