fe6c0e1e554cd271f4d837c633df5d97ff2248a7
Marcos Spomberg <marcosspomberg@gmail.com>
2022-05-16 22:58:45 +0000

    Create search route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'donations#index'
 
-  resources :donations, only: [:index, :show, :new, :edit]
+  resources :donations, only: [:index, :show, :new, :edit, :search]
 
   resources :users, only: [:index, :show]
 
@@ -11,9 +11,10 @@ Rails.application.routes.draw do
   get '/donations' => 'donations#index'
   get '/users' => 'users#index'
   get '/comments' => 'comments#index'
-
+  
   post '/add-donation' => 'donations#new'
   post '/claim-donation' => 'donations#edit'
   post '/add-comment' => 'comments#new'
-
+  post '/donations/search' => 'donations#search' 
+  
 end
