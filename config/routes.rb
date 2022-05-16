123ea3120f2a44dcf05a393f35fbb8e861863684
Marcos Spomberg <marcosspomberg@gmail.com>
2022-05-16 00:54:14 +0000

    Fix add-comment route

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,6 @@ Rails.application.routes.draw do
 
   post '/add-donation' => 'donations#new'
   post '/claim-donation' => 'donations#edit'
-  post 'add-comment' => 'comments#new'
+  post '/add-comment' => 'comments#new'
 
 end
