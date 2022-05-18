605e4d54ec71eea5efc57753cf4dc5b2a6d47cad
Marcos Spomberg <marcosspomberg@gmail.com>
2022-05-18 21:19:50 +0000

    Set up Google Maps Api, create maps helper functions

--- app/helpers/donations_helper.rb
+++ app/helpers/donations_helper.rb
@@ -1,2 +1,16 @@
 module DonationsHelper
+  
+  Google::Maps.configure do |config|
+    config.authentication_mode = Google::Maps::Configuration::API_KEY
+    config.api_key = ENV['GOOGLE_MAPS_API_KEY']
+  end
+
+  def coordinates_to_string latitude, longitude
+    "#{latitude},#{longitude}"
+  end
+
+  def calculate_distance location1, location2
+    Google::Maps.route(location1, location2).distance.value
+  end
+
 end
