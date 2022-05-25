a7786db41dacb82185d1fce25690d958a7c6262c
Tony <awalker780@gmail.com>
2022-05-24 21:22:14 -0400

    Add new seeds to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,37 +38,249 @@ end
 products = (HTTP.get("https://fakestoreapi.com/products")).parse
 conditions = ["Like New", "Good", "Acceptable", "Broken"]
 
-20.times do |index|
-  users[rand(0..4)].donations.create!(
-    name: products[index]['title'], 
+# 20.times do |index|
+#   users[rand(0..4)].donations.create!(
+#     name: products[index]['title'], 
+#     location: Faker::Address.street_address, 
+#     description: products[index]['description'],
+#     condition: conditions[rand(0..3)],
+#     available: true,
+#     image: products[index]['image'],
+#     created_at: Faker::Time.backward(days: 2),
+#     latitude: generate_toronto_coordinates[0],
+#     longitude: generate_toronto_coordinates[1]
+#   )
+# end
+
+users[rand(0..4)].donations.create!(
+    name: "Hot Tub", 
     location: Faker::Address.street_address, 
     description: products[index]['description'],
     condition: conditions[rand(0..3)],
     available: true,
-    image: products[index]['image'],
+    image: "",
     created_at: Faker::Time.backward(days: 2),
     latitude: generate_toronto_coordinates[0],
     longitude: generate_toronto_coordinates[1]
-  )
-end
-
+)
 users[rand(0..4)].donations.create!(
-    name: "Seville Tower Fan", 
+    name: "Miscellaneous DVDs", 
     location: Faker::Address.street_address, 
-    description: "Free Tower Fan! Still works - no need for it anymore.",
+    description: products[index]['description'],
     condition: conditions[rand(0..3)],
     available: true,
-    image: "https://drive.google.com/file/d/14Upf2PBlQwU1AJWO5GhUp6UVwOoI1JXb/view?usp=sharing",
+    image: "",
+    created_at: Faker::Time.backward(days: 2),
+    latitude: generate_toronto_coordinates[0],
+    longitude: generate_toronto_coordinates[1]
+)
+users[rand(0..4)].donations.create!(
+    name: "Vintage Freezer Door", 
+    location: Faker::Address.street_address, 
+    description: products[index]['description'],
+    condition: conditions[rand(0..3)],
+    available: true,
+    image: "",
+    created_at: Faker::Time.backward(days: 2),
+    latitude: generate_toronto_coordinates[0],
+    longitude: generate_toronto_coordinates[1]
+)
+users[rand(0..4)].donations.create!(
+    name: "Well-Loved Couch", 
+    location: Faker::Address.street_address, 
+    description: products[index]['description'],
+    condition: conditions[rand(0..3)],
+    available: true,
+    image: "",
+    created_at: Faker::Time.backward(days: 2),
+    latitude: generate_toronto_coordinates[0],
+    longitude: generate_toronto_coordinates[1]
+)
+users[rand(0..4)].donations.create!(
+    name: "Women's Shoes + Wine Rack", 
+    location: Faker::Address.street_address, 
+    description: products[index]['description'],
+    condition: conditions[rand(0..3)],
+    available: true,
+    image: "",
+    created_at: Faker::Time.backward(days: 2),
+    latitude: generate_toronto_coordinates[0],
+    longitude: generate_toronto_coordinates[1]
+)
+users[rand(0..4)].donations.create!(
+    name: "Custom Mahogany Shelf", 
+    location: Faker::Address.street_address, 
+    description: products[index]['description'],
+    condition: conditions[rand(0..3)],
+    available: true,
+    image: "",
+    created_at: Faker::Time.backward(days: 2),
+    latitude: generate_toronto_coordinates[0],
+    longitude: generate_toronto_coordinates[1]
+)
+users[rand(0..4)].donations.create!(
+    name: "Baby Thermos", 
+    location: Faker::Address.street_address, 
+    description: products[index]['description'],
+    condition: conditions[rand(0..3)],
+    available: true,
+    image: "",
+    created_at: Faker::Time.backward(days: 2),
+    latitude: generate_toronto_coordinates[0],
+    longitude: generate_toronto_coordinates[1]
+)
+users[rand(0..4)].donations.create!(
+    name: "Four Drawer Ikea Unit", 
+    location: Faker::Address.street_address, 
+    description: products[index]['description'],
+    condition: conditions[rand(0..3)],
+    available: true,
+    image: "",
+    created_at: Faker::Time.backward(days: 2),
+    latitude: generate_toronto_coordinates[0],
+    longitude: generate_toronto_coordinates[1]
+)
+users[rand(0..4)].donations.create!(
+    name: "70's Art Deco Chairs + Coffee Table", 
+    location: Faker::Address.street_address, 
+    description: products[index]['description'],
+    condition: conditions[rand(0..3)],
+    available: true,
+    image: "",
+    created_at: Faker::Time.backward(days: 2),
+    latitude: generate_toronto_coordinates[0],
+    longitude: generate_toronto_coordinates[1]
+)
+users[rand(0..4)].donations.create!(
+    name: "Nintendo Switch Gameboy", 
+    location: Faker::Address.street_address, 
+    description: products[index]['description'],
+    condition: conditions[rand(0..3)],
+    available: true,
+    image: "",
     created_at: Faker::Time.backward(days: 2),
     latitude: generate_toronto_coordinates[0],
     longitude: generate_toronto_coordinates[1]
 )
 
 users[rand(0..4)].donations.create!(
-    name: "Beige Couch", 
+    name: "Beige Couch with Detachable Feet - Wow!", 
     location: Faker::Address.street_address, 
-    description: "Well worn couch - up for grabs!",
-    condition: conditions[3],
+    description: products[index]['description'],
+    condition: conditions[rand(0..3)],
+    available: true,
+    image: "",
+    created_at: Faker::Time.backward(days: 2),
+    latitude: generate_vancouver_coordinates[0],
+    longitude: generate_vancouver_coordinates[1]
+)
+users[rand(0..4)].donations.create!(
+    name: "China Cabinet (Doge not included)", 
+    location: Faker::Address.street_address, 
+    description: products[index]['description'],
+    condition: conditions[rand(0..3)],
+    available: true,
+    image: "",
+    created_at: Faker::Time.backward(days: 2),
+    latitude: generate_vancouver_coordinates[0],
+    longitude: generate_vancouver_coordinates[1]
+)
+users[rand(0..4)].donations.create!(
+    name: "Locked Filing Cabinet (Key not included)", 
+    location: Faker::Address.street_address, 
+    description: products[index]['description'],
+    condition: conditions[rand(0..3)],
+    available: true,
+    image: "",
+    created_at: Faker::Time.backward(days: 2),
+    latitude: generate_vancouver_coordinates[0],
+    longitude: generate_vancouver_coordinates[1]
+)
+users[rand(0..4)].donations.create!(
+    name: "'Tweed like your Professor's Jacket' Chair", 
+    location: Faker::Address.street_address, 
+    description: products[index]['description'],
+    condition: conditions[rand(0..3)],
+    available: true,
+    image: "",
+    created_at: Faker::Time.backward(days: 2),
+    latitude: generate_vancouver_coordinates[0],
+    longitude: generate_vancouver_coordinates[1]
+)
+users[rand(0..4)].donations.create!(
+    name: "Walmart Billy Bookshelf", 
+    location: Faker::Address.street_address, 
+    description: products[index]['description'],
+    condition: conditions[rand(0..3)],
+    available: true,
+    image: "",
+    created_at: Faker::Time.backward(days: 2),
+    latitude: generate_vancouver_coordinates[0],
+    longitude: generate_vancouver_coordinates[1]
+)
+users[rand(0..4)].donations.create!(
+    name: "Itsy Bitsy Teeny Weeny Wooden Table", 
+    location: Faker::Address.street_address, 
+    description: products[index]['description'],
+    condition: conditions[rand(0..3)],
+    available: true,
+    image: "",
+    created_at: Faker::Time.backward(days: 2),
+    latitude: generate_vancouver_coordinates[0],
+    longitude: generate_vancouver_coordinates[1]
+)
+users[rand(0..4)].donations.create!(
+    name: "Velma Skirt Orange Couch", 
+    location: Faker::Address.street_address, 
+    description: products[index]['description'],
+    condition: conditions[rand(0..3)],
+    available: true,
+    image: "",
+    created_at: Faker::Time.backward(days: 2),
+    latitude: generate_vancouver_coordinates[0],
+    longitude: generate_vancouver_coordinates[1]
+)
+users[rand(0..4)].donations.create!(
+    name: "Haunted TV", 
+    location: Faker::Address.street_address, 
+    description: products[index]['description'],
+    condition: conditions[rand(0..3)],
+    available: true,
+    image: "",
+    created_at: Faker::Time.backward(days: 2),
+    latitude: generate_vancouver_coordinates[0],
+    longitude: generate_vancouver_coordinates[1]
+)
+
+users[rand(0..4)].donations.create!(
+    name: "Big Comfy Couch", 
+    location: Faker::Address.street_address, 
+    description: products[index]['description'],
+    condition: conditions[rand(0..3)],
+    available: true,
+    image: "",
+    created_at: Faker::Time.backward(days: 2),
+    latitude: generate_vancouver_coordinates[0],
+    longitude: generate_vancouver_coordinates[1]
+)
+
+users[rand(0..4)].donations.create!(
+    name: "Free Time Machine!!", 
+    location: Faker::Address.street_address, 
+    description: products[index]['description'],
+    condition: conditions[rand(0..3)],
+    available: true,
+    image: "",
+    created_at: Faker::Time.backward(days: 2),
+    latitude: generate_vancouver_coordinates[0],
+    longitude: generate_vancouver_coordinates[1]
+)
+
+users[rand(0..4)].donations.create!(
+    name: "Danny Duncan Original Dirtbike", 
+    location: Faker::Address.street_address, 
+    description: products[index]['description'],
+    condition: conditions[rand(0..3)],
     available: true,
     image: "",
     created_at: Faker::Time.backward(days: 2),
